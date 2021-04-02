f00a0dcb3be917119278dcc636b9e60e2b7f5969
Jace Riehl <jace.riehl@uleth.ca>
2021-04-02 16:38:47 -0600

    Beginning adding the drawing elements

--- js/scripts.js
+++ js/scripts.js
@@ -26,17 +26,26 @@ function draw() {
 	translate(vidcapture.width, 0)
 	scale(-1, 1)
 
-	image(vidcapture, 0, 0)
+	// image(vidcapture, 0, 0)
 
 
 	var positions = ctracker.getCurrentPosition()
 
 	if(positions) {
 		// ctracker.draw(drawcanvas)
-		fill(0)
-		ellipse(positions[62][0], positions[62][1], 20)
-		line(positions[23][0], positions[23][1], positions[23][0]-30, positions[23][1]-30)
-		line(positions[28][0], positions[28][1], positions[28][0]+30, positions[28][1]-30)
+		fill($("#color").val())
+		print($("#color").val())
+		if($("#object").val() == "line") {
+			line(positions[23][0], positions[23][1], positions[23][0]-30, positions[23][1]-30)
+		} else if($("#object").val() == "ellipse") {
+			ellipse(positions[62][0], positions[62][1], 20)
+		} else {
+			rect(positions[62][0], positions[62][1], 20, 20)
+		}
+		
+		// line(positions[23][0], positions[23][1], positions[23][0]-30, positions[23][1]-30)
+		// line(positions[28][0], positions[28][1], positions[28][0]+30, positions[28][1]-30)
+		// image("imgs/test.png", 50, 50, 20, 20)
 
 		// var r = map(positions[62][0], 0, width, 0, 255, true)
 		// var b = map(positions[62][1], 0, height, 0, 255, true)
