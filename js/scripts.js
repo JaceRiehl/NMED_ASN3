4f44a5dcc9e05cf43043b6dca1f2f5de35e07251
Jace Riehl <jace.riehl@uleth.ca>
2021-04-07 14:44:42 -0600

    Fixing camera width

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ function setup() {
 	cnv.parent('p5Canvas')
 
 	vidcapture = createCapture(VIDEO)
-	vidcapture.size(vidcapture.width*3, vidcapture.height*4.5)
+	vidcapture.size(windowWidth, windowHeight * 5/8)
 	vidcapture.hide()
 
 	ctracker = new clm.tracker()
@@ -21,13 +21,14 @@ function setup() {
 
 function draw() {
 
-	// imageMode(CORNERS)
 	translate(vidcapture.width, 0)
 	scale(-1, 1)
+	print(windowWidth)
 	if($('#camera').val() == "on") {
 		background(255,255,255)
-		print((vidcapture.width*3)/2)
-		image(vidcapture, -windowWidth + (vidcapture.width*3)/2, 0)
+		// print((vidcapture.width*3)/2)
+		// image(vidcapture, -windowWidth + (vidcapture.width*3)/2, 0)
+		image(vidcapture, 0, 0)
 		flag = true
 	} else if($('#camera').val() == "off" && flag){
 		background(255,255,255)
@@ -54,8 +55,8 @@ function draw() {
 			cursor = 60
 		}
 		
-		var x = map(positions[cursor][0], 560, 340, windowWidth/2, 0, true)
-		var y = map(positions[cursor][1], 320, 220, windowHeight * 5/8, 0, true)
+		var x = map(positions[cursor][0], windowWidth/2+300, windowWidth/2-300, windowWidth, 0, false)
+		var y = map(positions[cursor][1], ((windowHeight * 5/8)/2)+100, ((windowHeight * 5/8)/2)-100, windowHeight * 5/8, 0, false)
 
 
 		fill($("#color").val())
